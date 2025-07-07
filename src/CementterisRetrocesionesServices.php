aa6f56a654e1de685017a7a4851607dba748b2b7
Maluluu <mcastro@emergya.com>
2025-07-07 15:42:16 +0200

    Add services

---
+++ src/CementterisRetrocesionesServices.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Drupal\cementeris_retrocesiones;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Cache\CacheFactoryInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Session\AccountInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Service description.
+ */
+class CementterisRetrocesionesServices {
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The cache.backend.database service.
+   *
+   * @var \Drupal\Core\Cache\CacheFactoryInterface
+   */
+  protected $cacheBackendDatabase;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $account;
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Constructs a CementterisRetrocesionesServices object.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   * @param \Drupal\Core\Cache\CacheFactoryInterface $cache_backend_database
+   *   The cache.backend.database service.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The current user.
+   * @param \GuzzleHttp\ClientInterface $client
+   *   The HTTP client.
+   */
+  public function __construct(LoggerInterface $logger, MessengerInterface $messenger, CacheFactoryInterface $cache_backend_database, AccountInterface $account, ClientInterface $client) {
+    $this->logger = $logger;
+    $this->messenger = $messenger;
+    $this->cacheBackendDatabase = $cache_backend_database;
+    $this->account = $account;
+    $this->httpClient = $client;
+  }
+
+  /**
+   * Method description.
+   */
+  public function postCementiris() {
+    try {
+
+      $response = $this->httpClient->request('POST', 'https://accfun.cbsa.cat/gas/ws/r/RetroSolicPRE');
+      return Json::decode($response->getBody()->getContents());
+    }
+    catch (ConnectException $e) {
+      $this->customHandlerExceptionMessage($e);
+      $this->send_email('No se pudo conectar al webservice.');
+      return [];
+    }
+    catch (RequestException $e) {
+      $this->logger->error('Comunication Error in PostCementiris, ' . $e);
+      $this->customHandlerExceptionMessage($e);
+      return [];
+    }
+
+  }
+
+  /**
+   * @param $error
+   *
+   * @return void
+   */
+  public function send_email($error): void {
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    $module = 'cementeris_retrocesiones';
+    $key = 'webservice_error';
+    $to = 'soporte@bsm.com';
+    $params['subject'] = 'Error de conexión con el WebService';
+    $params['message'] = "Ocurrió un error al conectar con el WebService:\n\n$error";
+    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $send = true;
+
+    $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+  }
+
+  /**
+   * Method error message.
+   *
+   * @param $e
+   * @return void
+   */
+  protected function customHandlerExceptionMessage($e): void
+  {
+    $this->logger->error($e);
+    // Show messages only for administrator role
+    if (in_array('administrator', $this->account->getRoles())) {
+      if ($e instanceof RequestException && $e->hasResponse()) {
+        try {
+          $error_content = Json::decode($e->getResponse()->getBody()->getContents());
+          $this->messenger->addWarning($error_content);
+        } catch (\Exception $jsonException) {
+          $this->messenger->addWarning('An error occurred: ' . $e->getMessage());
+        }
+      } else {
+        $this->messenger->addWarning('An error occurred: ' . $e->getMessage());
+      }
+    }
+  }
+
+}
+
+
--- src/Controller/CementerisRetrocesionesController.php
+++ src/Controller/CementerisRetrocesionesController.php
@@ -16,13 +16,20 @@ class CementerisRetrocesionesController extends ControllerBase {
 
     $build['title'] = [
       '#type' => 'markup',
-      '#markup' => $this->t('description'),
+      '#markup' => $this->t('Retrocessions'),
+      '#prefix' => '<div class="title-page"><h3>',
+      '#suffix' => '</h3></div>',
     ];
     $build['description'] = [
       '#type' => 'markup',
       '#markup' => $this->t('Vivamus sollicitudin leo eget dui placerat vehicula. Aenean ultrices euismod arcu sed finibus. Vivamus commodo erat sed mauris aliquam, non pellentesque lorem bibendum. Etiam sit amet dolor at velit consectetur tincidunt. Ut ultrices pretium tellus id pretium. Sed interdum bibendum tortor, vitae dictum tortor fringilla vel. Duis ut ultrices lectus, at viverra tellus. Proin aliquam commodo porta. Ut ac sagittis enim, non gravida augue. Praesent a urna eget enim rutrum luctus. Aenean odio ipsum, tempus sit amet orci nec, tristique elementum dolor. Ut justo quam, placerat in convallis eu, sodales vitae augue. Phasellus ac dolor porta, commodo ex non, pellentesque sem. Donec purus nisi, gravida bibendum ullamcorper nec, gravida ac velit.'),
       '#prefix' => '<div class="page-information"><p>',
       '#suffix' => '</p></div>',
+      '#attached' => [
+        'library' => [
+          'cementeris_retrocesiones/cementeris_retrocesiones',
+        ],
+      ],
     ];
 
     return $build;
@@ -32,13 +39,20 @@ class CementerisRetrocesionesController extends ControllerBase {
 
     $build['content'] = [
       '#type' => 'markup',
-      '#markup' => $this->t('information'),
+      '#markup' => $this->t('Information'),
+      '#prefix' => '<div class="title-page"><h3>',
+      '#suffix' => '</h3></div>',
     ];
     $build['description'] = [
       '#type' => 'markup',
       '#markup' => $this->t('Quisque odio nisl, tempus sed imperdiet a, rhoncus commodo purus. Mauris quis dapibus orci. In semper congue sem, eu faucibus purus iaculis eu. Nulla risus urna, ornare a sem rhoncus, iaculis sollicitudin ex. Cras at convallis velit, eget egestas augue. Nulla massa ligula, vehicula eu erat vel, rhoncus bibendum erat. In interdum erat tortor, in mollis tortor ullamcorper ac. Morbi eget semper erat. Suspendisse ultrices aliquam lacus. Duis pharetra nulla nec posuere placerat. Morbi nec nisl sit amet lacus cursus ornare non ut urna. Maecenas quam quam, rhoncus non venenatis in, commodo et sapien. Duis in convallis ligula.'),
       '#prefix' => '<div class="page-information"><p>',
       '#suffix' => '</p></div>',
+      '#attached' => [
+        'library' => [
+          'cementeris_retrocesiones/cementeris_retrocesiones',
+        ],
+      ],
     ];
 
     return $build;
